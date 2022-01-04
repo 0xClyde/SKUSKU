7004e4a4c8fd4d2ac5211abd508dfd16e9a1708a
0xClyde <90419588+0xClyde@users.noreply.github.com>
2022-01-04 19:02:13 +0100

    Comments for The Devine Farm
    Made some comments to make the code easier to read :) :100:  :1st_place_medal:

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -55,7 +55,7 @@ contract SupplyChain {
    * Modifiers
    */
 
-  // Create a modifer, `isOwner` that checks if the msg.sender is the owner of the contract
+  //  `isOwner` checks if the msg.sender is the owner of the contract
 
   // <modifier: isOwner
 
@@ -82,9 +82,7 @@ contract SupplyChain {
     items[_sku].buyer.transfer(amountToRefund);
   }
 
-  // For each of the following modifiers, use what you learned about modifiers
-  // to give them functionality. For example, the forSale modifier should
-  // require that the item with the given sku has the state ForSale. Note that
+  //  Note that:
   // the uninitialized Item.State is 0, which is also the index of the ForSale
   // value, so checking that Item.State == ForSale is not sufficient to check
   // that an Item is for sale. Hint: What item properties will be non-zero when
@@ -97,7 +95,7 @@ contract SupplyChain {
 
   }
   modifier sold(uint _sku) {
-      require(items[_sku].state == State.Sold, "It's not Sold Sold");
+      require(items[_sku].state == State.Sold, "It's not Sold ");
       _;
   }
   modifier shipped(uint _sku) {
@@ -143,6 +141,7 @@ contract SupplyChain {
   //     - check the value after the function is called to make 
   //      sure the buyer is refunded any excess ether sent. 
   // 6. call the event associated with this function!
+
   function buyItem(uint sku) public payable forSale(sku)  {
 
       items[sku].buyer = payable(msg.sender);
@@ -160,6 +159,7 @@ contract SupplyChain {
   //    - the person calling this function is the seller. 
   // 2. Change the state of the item to shipped. 
   // 3. call the event associated with this function!
+
   function shipItem(uint sku) public sold(sku) verifyCaller(items[sku].seller){
 
       items[sku].state = State.Shipped;
@@ -174,12 +174,14 @@ contract SupplyChain {
   //    - the person calling this function is the buyer. 
   // 2. Change the state of the item to received. 
   // 3. Call the event associated with this function!
+
   function receiveItem(uint sku) public  shipped(sku) verifyCaller(items[sku].buyer){
       items[sku].state = State.Received;
       emit LogReceived(sku);
   }
 
-  // Uncomment the following code block. it is needed to run tests
+  // This function is necessary for running the tests only
+
     function fetchItem(uint _sku) public view  
   returns (string memory name, uint sku, uint price, uint state, address seller, address buyer)  
   { 
